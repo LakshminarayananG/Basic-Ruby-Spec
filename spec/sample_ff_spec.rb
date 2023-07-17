e0af7a633361e2ff5a3c34455d4ffa247b900daa
Lakshminarayanan Ganesan <lakshnarayanan7@gmail.com>
2023-07-17 20:32:02 +0530

    Update sample_ff_spec.rb

--- spec/sample_ff_spec.rb
+++ spec/sample_ff_spec.rb
@@ -18,12 +18,32 @@ describe "Sample web test" do
 		@driver.quit
 	end
 
-	it "Login to Leaftaps Application" do
+	it "Launch salesforce Login Page" do 
+
+			#Login to leaftaps application
 			@driver.navigate.to "http://login.salesforce.com"
-			sleep 30
-			@driver.find_element(:id => "username").send_keys "laksvijay@gmail.com"
+			wait = Selenium::WebDriver::Wait.new(:timeout => 40)
+			sleep 10
+			@driver.find_element(:id => "username").send_keys "laksvijay07@gmail.com"
 			@driver.find_element(:id => "password").send_keys "test@123"
 			@driver.find_element(:id => "Login").click
-		sleep 5
+		
+
+			#Click on the switcher and navigate to accounts section
+			@driver.find_element(:class => "slds-icon-waffle").click
+			@driver.find_element(:xpath => "//p[text()='Service']").click
+
+			wait.until { @driver.find_element(:xpath => "//span[text()='Accounts']") }
+			element = @driver.find_element(:xpath => "//span[text()='Accounts']")
+			@driver.execute_script("arguments[0].click();", element)
+			
+
+			#Click on new and create a new account
+			@driver.find_element(:xpath => "//div[@title='New']").click
+			acc_name = Faker::Name.unique.name
+			puts acc_name
+			@driver.find_element(:xpath => "//input[(@class='slds-input') and @name='Name']").send_keys acc_name
+
+			sleep 5
 	end
 end
